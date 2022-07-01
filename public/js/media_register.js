5e0a87cc542f9e729ff814b41c46d35345acd5fe
Thiago Bauce <thiedbauce@gmail.com>
2022-07-01 08:55:47 -0400

    botao maneiro do hori

---
+++ public/js/media_register.js
@@ -0,0 +1,16 @@
+function showMedia() {
+    const selectedElement = document.querySelector('#media').selectedIndex
+    const display = selectedElement ? 'none' : 'flex'
+    document.getElementById('spanInfo5').textContent = selectedElement == 2 ? 'Autor(a)' : 'Elenco'
+    document.getElementById('inputInfo5').setAttribute('name', selectedElement == 2 ? 'author' : 'cast')
+    document.getElementById('spanInfo6').style.display = display
+    document.getElementById('inputInfo6').style.display = display
+
+    const form = document.getElementById('mediaForm')
+    const action = ['filme', 'serie', 'livro'] 
+    form.action = 'http://localhost:8080/app/views/media/media_register_page/' + action[selectedElement]
+}
+
+function cancel() {
+    window.location.assign("../../../index.php")
+} 
