fa8c8e7d60d04e4d7b6e9d27ee546763a4eadcf4
Thiago Bauce <thiedbauce@gmail.com>
2022-07-01 11:58:37 -0400

    route update

--- app/views/navbar.php
+++ app/views/navbar.php
@@ -6,9 +6,8 @@
     <div class="nav__menu">
         <a href="<?= BASEPATH  ?>home" target="_parent" class="menu__item">Home</a>
         <a href="<?= BASEPATH  ?>buscaFilmes" target="_parent" class="menu__item">Filmes</a>
-        <a href="/minhacritica2.0/app/views/media/series_search_page.php" target="_parent" class="menu__item">Séries</a>
-        <a href="/minhacritica2.0/app/views/media/books_search_page.php" target="_parent" class="menu__item">Livros</a>
-        <a href="/minhacritica2.0/app/views/media/movie_review_filter.php" target="_parent" class="menu__item">Busca Avançada</a>
+        <a href="<?= BASEPATH  ?>buscaSeries" target="_parent" class="menu__item">Séries</a>
+        <a href="<?= BASEPATH  ?>buscaLivros" target="_parent" class="menu__item">Livros</a>
         <a href="/minhacritica2.0/app/views/user/kanban-usuario.php" target="_parent" class="menu__item">Kanban</a>
     </div>
 
