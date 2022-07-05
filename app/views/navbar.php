9c723f7f8aaaea0cb650cfb5c2729d1a513a451e
M3T4Z0D <lucas.cabral.cg18@gmail.com>
2022-07-05 00:20:21 -0400

    atualização do menu drop down

--- app/views/navbar.php
+++ app/views/navbar.php
@@ -33,11 +33,9 @@
         } else {
         ?>
             <div class="dropdown">
-                <button class="dropbtn"><?php  echo $loggedUser->nome ?> <i class="fa-solid fa-caret-down"></i></button>
+                <button class="dropbtn"><i class="fa-solid fa-user"></i> <?php  echo $loggedUser->nome ?> <i class="fa-solid fa-caret-down"></i></button>
                 <div class="dropdown-content">
-                    <a href="#">Link 1</a>
-                    <a href="#">Link 2</a>
-                    <a href="#">Link 3</a>
+                    <a href="#">Perfil</a>
                     <a href="<?= BASEPATH  ?>logout" target="_parent">Sair</a>
                 </div>
             </div>
